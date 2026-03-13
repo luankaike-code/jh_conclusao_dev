48df97833abb9583e1975c31822cf94e48685fd3
luankaike-code <luankaikemacksantos@gmail.com>
2026-03-13 14:48:16 -0300

    Make a demo menu

--- code-tcc-jh/scenes/worlds/menu/menu_world.gd
+++ code-tcc-jh/scenes/worlds/menu/menu_world.gd
@@ -1,17 +1,30 @@
 extends Node3D
 
-@onready var main_menu_panel_3d: MainMenuPanel3D = $main_menu_panel_3D
+@onready var main_menu_panel_3d: MainMenuPanel3D = $menus/main_menu_panel_3D
+@onready var camera: Camera3D = $Camera3D
+@onready var menu_configuration_path: Path3D = $paths/MenuConfiguration
+
+var path_to_follow: Path3D
+var current_point_index := 0
+var points_count: float
 
 func _ready() -> void:
 	main_menu_panel_3d.start_button_up.connect(_on_start_button_up)
 	main_menu_panel_3d.configuration_button_up.connect(_on_configuration_button_up)
 	main_menu_panel_3d.credits_button_up.connect(_on_credits_button_up)
 
+func _process(delta: float) -> void:
+	if path_to_follow:
+		points_count += 0.01
+		var pos := path_to_follow.curve.sample_baked_with_rotation(points_count)
+		camera.global_position = ((path_to_follow.global_position + pos.origin))
+		camera.basis = pos.basis
+
 func _on_start_button_up():
-	print("start")
+	get_tree().change_scene_to_file("uid://dhrvq381488f3")
 
 func _on_configuration_button_up():
-	print("configurações")
+	path_to_follow = menu_configuration_path
 
 func _on_credits_button_up():
 	print("creditos")
