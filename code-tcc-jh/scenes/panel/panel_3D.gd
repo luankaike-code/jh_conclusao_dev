86cb1254d83e44efd93501d790eaf6a9906022c8
luankaike-code <luankaikemacksantos@gmail.com>
2026-03-13 12:11:12 -0300

    Rename sub_viewport to viewport

--- code-tcc-jh/scenes/panel/panel_3D.gd
+++ code-tcc-jh/scenes/panel/panel_3D.gd
@@ -21,7 +21,7 @@ var is_mouse_inside: bool
 var last_event_pos_2d: Vector2 
 var last_event_time: float = -1
 
-@onready var sub_viewport: SubViewport = $SubViewport
+@onready var viewport: SubViewport = $SubViewport
 @onready var area_3d: Area3D = $MeshInstance3D/Area3D
 @onready var area_3d_collision: CollisionShape3D = $MeshInstance3D/Area3D/CollisionShape3D
 @onready var mesh_3d: MeshInstance3D = $MeshInstance3D
@@ -39,8 +39,8 @@ func _ready() -> void:
 		set_viewport_content(default_viewport_child)
 
 func _update_viewport_size():
-	if sub_viewport:
-		sub_viewport.size = viewport_size
+	if viewport:
+		viewport.size = viewport_size
 
 func _update_mesh_size():
 	if area_3d_collision && mesh_3d:
@@ -57,7 +57,7 @@ func _unhandled_input(event: InputEvent) -> void:
 	for mouse_event in [InputEventMouseButton, InputEventMouseMotion, InputEventScreenDrag, InputEventScreenTouch]:
 		if is_instance_of(event, mouse_event): # if event is a mouse event
 			return
-	sub_viewport.push_input(event)
+	viewport.push_input(event)
 
 func _on_event_input_area(_camera: Camera3D, event: InputEvent, event_position: Vector3, _normal: Vector3, _shape_idx: int):
 	var now_event_time: float = Time.get_ticks_msec() / 1000.0
@@ -73,7 +73,7 @@ func _on_event_input_area(_camera: Camera3D, event: InputEvent, event_position: 
 		event_pos_2d += Vector2.ONE / 2
 		# event_pos_2d.d = Range(0, 1)
 		
-		event_pos_2d *= Vector2(sub_viewport.size)
+		event_pos_2d *= Vector2(viewport.size)
 	else:
 		event_pos_2d = last_event_pos_2d
 	
@@ -88,18 +88,18 @@ func _on_event_input_area(_camera: Camera3D, event: InputEvent, event_position: 
 	last_event_pos_2d = event_pos_2d
 	last_event_time = now_event_time
 	
-	sub_viewport.push_input(event)
+	viewport.push_input(event)
 	
 func set_viewport_content(packed_scene: PackedScene) -> CanvasItem:
-	if !sub_viewport:
+	if !viewport:
 		return
 	
 	var scene: CanvasItem = packed_scene.instantiate()
 	
-	for sub_viewport_child in sub_viewport.get_children():
-		sub_viewport_child.queue_free()
+	for viewport_child in viewport.get_children():
+		viewport_child.queue_free()
 		
-	sub_viewport.add_child(scene)
+	viewport.add_child(scene)
 	return scene
 
 	
